260dc26ec12568bf00f5a84ba6f8042298089d09
yumetodo <yume-wikijp@live.jp>
2019-08-19 00:08:50 +0900

    fix: try to parse HTML using google-photos-album-image-url-fetch
    to avoid using baseUrl
    
    ref:
    - #2

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,13 @@ import * as path from 'path';
 import { promises as fs } from 'fs';
 import AbortController from 'abort-controller';
 import { markdownImgUrlEditor } from 'markdown_img_url_editor';
+import { GooglePhotos } from 'google-photos-album-image-url-fetch';
 import { Configuration } from './configuration';
 import { AuthManager } from './authManager';
 import { Photos } from './googlePhotos';
 import { waitFor } from './timer';
 import { selectTargetAlbum } from './selectTargetAlbum';
+// const getUrl = (r: Photos.MediaItemResult) => (r.mediaItem ? r.mediaItem.baseUrl : undefined);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
@@ -18,16 +20,16 @@ export async function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand('google-photos-uploader.upload', async () => {
-    vscode.window.showErrorMessage(
-      'Currently, to get img url, we are using `baseUrl` that is valid **only for 60 minutes**',
-      'DO NOT USE THIS PLUGIN UNTIL THIS BUG IS TO BE FIXED'
-    );
-    const continueFlag = await vscode.window.showQuickPick(['YES(NOT RECOMMENDED'], {
-      placeHolder: 'Are you really continue process?',
-    });
-    if (!continueFlag || continueFlag !== 'YES(NOT RECOMMENDED') {
-      return;
-    }
+    // vscode.window.showErrorMessage(
+    //   'Currently, to get img url, we are using `baseUrl` that is valid **only for 60 minutes**',
+    //   'DO NOT USE THIS PLUGIN UNTIL THIS BUG IS TO BE FIXED'
+    // );
+    // const continueFlag = await vscode.window.showQuickPick(['YES(NOT RECOMMENDED'], {
+    //   placeHolder: 'Are you really continue process?',
+    // });
+    // if (!continueFlag || continueFlag !== 'YES(NOT RECOMMENDED') {
+    //   return;
+    // }
     if (!vscode.window.activeTextEditor) {
       vscode.window.showErrorMessage('Please open target markdown text file.');
       return;
@@ -48,93 +50,117 @@ export async function activate(context: vscode.ExtensionContext) {
       const targetAlbum = await selectTargetAlbum(AbortControllerMap, photos);
       if (null === targetAlbum) {
         return;
-              }
-        const selectedTabFilePath = vscode.window.activeTextEditor.document.fileName;
-        const dir = path.dirname(selectedTabFilePath);
-        const tokenGetters: Array<Promise<[string, string]>> = [];
-        const uploadingImagePath = new Map<string, number>();
-        let urls: (string | undefined)[] = [];
-        let timer = waitFor(1);
-        const replaced = await markdownImgUrlEditor(
-          text,
-          (alt: string, s: string) => {
-            const p = path.resolve(dir, s);
-            //avoid duplicate upload
-            if (uploadingImagePath.has(p)) {
-              const index = uploadingImagePath.get(p);
-              return () => (index ? urls[index] || s : s);
-            } else {
-              const k = `upload::${p}`;
-              const c = new AbortController();
-              AbortControllerMap.set(k, c);
-              const index =
-                tokenGetters.push(
-                  timer
-                    .then(_ => photos.upload(p, c.signal))
-                    .then(t => {
-                      timer = waitFor(300);
-                      AbortControllerMap.delete(k);
-                      return [alt, t];
-                    })
-                ) - 1;
-              uploadingImagePath.set(p, index);
-              return () => urls[index] || s;
+      }
+      const selectedTabFilePath = vscode.window.activeTextEditor.document.fileName;
+      const dir = path.dirname(selectedTabFilePath);
+      const tokenGetters: Array<Promise<[string, string]>> = [];
+      const uploadingImagePath = new Map<string, number>();
+      const timestamps: Promise<number>[] = [];
+      let urls: (string | undefined)[] = [];
+      let timer = waitFor(1);
+      const replaced = await markdownImgUrlEditor(
+        text,
+        (alt: string, s: string) => {
+          const p = path.resolve(dir, s);
+          //avoid duplicate upload
+          if (uploadingImagePath.has(p)) {
+            const index = uploadingImagePath.get(p);
+            return () => (index ? urls[index] || s : s);
+          } else {
+            const k = `upload::${p}`;
+            timestamps.push(fs.stat(p).then(stat => stat.mtimeMs));
+            const c = new AbortController();
+            AbortControllerMap.set(k, c);
+            const index =
+              tokenGetters.push(
+                timer
+                  .then(_ => photos.upload(p, c.signal))
+                  .then(t => {
+                    timer = waitFor(300);
+                    AbortControllerMap.delete(k);
+                    return [alt, t];
+                  })
+              ) - 1;
+            uploadingImagePath.set(p, index);
+            return () => urls[index] || s;
+          }
+        },
+        async () => {
+          const tokens = await vscode.window.withProgress(
+            {
+              cancellable: true,
+              title: 'uploading images...',
+              location: vscode.ProgressLocation.Notification,
+            },
+            (_, token) => {
+              token.onCancellationRequested(onCancellationRequested);
+              return Promise.all(tokenGetters);
             }
-          },
-          async () => {
-            const tokens = await vscode.window.withProgress(
-              {
-                cancellable: true,
-                title: 'uploading images...',
-                location: vscode.ProgressLocation.Notification,
-              },
-              (_, token) => {
-                token.onCancellationRequested(onCancellationRequested);
-                return Promise.all(tokenGetters);
+          );
+          urls = await vscode.window.withProgress(
+            { cancellable: false, title: 'registering images...', location: vscode.ProgressLocation.Notification },
+            async progress => {
+              progress.report({ increment: 0 });
+              const re: (string | undefined)[] = [];
+              const fetchImageUrlsAbortController = new AbortController();
+              AbortControllerMap.set('fetchImageUrls', fetchImageUrlsAbortController);
+              let before = await GooglePhotos.Album.fetchImageUrls(
+                targetAlbum.shareableUrl,
+                fetchImageUrlsAbortController.signal
+              );
+              AbortControllerMap.delete('fetchImageUrls');
+              if (null === before) {
+                throw new Error('GooglePhotos.Album.fetchImageUrls fail');
               }
-            );
-            const batchCreateAbortController = new AbortController();
-            AbortControllerMap.set('batchCreate', batchCreateAbortController);
-            const res = await vscode.window.withProgress(
-              {
-                cancellable: false,
-                title: 'registering images...',
-                location: vscode.ProgressLocation.Notification,
-              },
-              () =>
-                photos.mediaItems.batchCreate(
+              for (let i = 0; i < tokens.length; ++i) {
+                const t = tokens[i];
+                const timestamp = await timestamps[i];
+                const batchCreateAbortController = new AbortController();
+                AbortControllerMap.set('batchCreate', batchCreateAbortController);
+                await photos.mediaItems.batchCreate(
                   {
-                    albumId: albumId,
-                    newMediaItems: tokens.map(
-                      (t): Photos.NewMediaItem => ({
+                    albumId: targetAlbum.albumId,
+                    newMediaItems: [
+                      {
                         simpleMediaItem: {
                           uploadToken: t[1],
                         },
                         description: t[0],
-                      })
-                    ),
+                      },
+                    ],
                   },
                   batchCreateAbortController.signal
-                )
-            );
-            AbortControllerMap.delete('batchCreate');
-            const ids = res.map(r => (r.mediaItem ? r.mediaItem.id : undefined));
-            const batchGetAbortController = new AbortController();
-            AbortControllerMap.set('batchGet', batchGetAbortController);
-            //The mediaItems info batchCreate return lacks baseUrl so that we need to get again using batchGet
-            const res2 = await photos.mediaItems.batchGet(
-              ids.filter((id: string | undefined): id is string => !(undefined === id)),
-              batchGetAbortController.signal
-            );
-            AbortControllerMap.delete('batchGet');
-            let i = 0;
-            urls = ids.map(id => (id ? getUrl(res2[i++]) : undefined));
-          }
-        );
-        textEditor.edit(builder => {
-          builder.replace(allRange, replaced);
-        });
-      }
+                );
+                AbortControllerMap.delete('batchCreate');
+                const fetchImageUrlsAbortController = new AbortController();
+                AbortControllerMap.set('fetchImageUrls', fetchImageUrlsAbortController);
+                const after = await GooglePhotos.Album.fetchImageUrls(
+                  targetAlbum.shareableUrl,
+                  fetchImageUrlsAbortController.signal
+                );
+                AbortControllerMap.delete('fetchImageUrls');
+                if (null === after) {
+                  throw new Error('GooglePhotos.Album.fetchImageUrls fail');
+                }
+                let appended = GooglePhotos.Album.extractAppended(before, after);
+                if (0 === appended.length) {
+                  throw new Error('Unexpected behavior was occurred while registering image');
+                }
+                if (1 < appended.length) {
+                  appended = appended.filter(e => e.imageUpdateDate === timestamp);
+                }
+                re.push(1 === appended.length ? appended[0].url : undefined);
+                before = after;
+                progress.report({ increment: (100 * i) / tokens.length });
+              }
+              return re;
+            }
+          );
+        }
+      );
+      textEditor.edit(builder => {
+        builder.replace(allRange, replaced);
+      });
     } catch (er) {
       if (!(er instanceof Error && er.name === 'AbortError')) {
         onCancellationRequested();
