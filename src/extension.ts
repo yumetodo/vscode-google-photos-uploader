d1f68f23fba2bb129f66ac9edb78f208848a0827
yumetodo <yume-wikijp@live.jp>
2019-09-21 13:22:03 +0900

    chore: simpliify message

--- src/extension.ts
+++ src/extension.ts
@@ -139,7 +139,7 @@ export async function activate(context: vscode.ExtensionContext) {
               t = waitFor(1000);
               progress.report({
                 increment: (100 * ++i) / tokenGetters.length,
-                message: `uploading images...(${i}/${tokenGetters.length})`,
+                message: `(${i}/${tokenGetters.length})`,
               });
             }
             await t;
